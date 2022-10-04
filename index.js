707f6b0dea4826319ea307a7ba9f783429cb3835
yasmin deodato <yasmin.deodato.beatriz@gmail.com>
2022-10-04 17:30:18 -0300

    create company register and set its endpoint

--- index.js
+++ index.js
@@ -133,6 +133,50 @@ app.get('/home', (req, res) => {
     });
 }) 
 
+/*------------COMPANY-------------*/
+app.post('/company/SignUp', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Methods", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+      );
+      
+    const nome = req.body.nome;
+    const email = req.body.email;
+    const senha = bcrypt.hashSync(req.body.senha, 3);
+    const cnpj = req.body.cnpj;
+    const telefone = req.body.telefone;
+    const select_query = 'SELECT * FROM COMPANY WHERE EMAIL = ?'
+    const insert_query = 'INSERT INTO COMPANY (NOME, EMAIL, SENHA, CNPJ, TELEFONE) VALUES (?, ?, ?, ?, ?)'
+    
+    connection.query(select_query, [email], (err, result) => {
+        if(err){
+            res.json(err);
+        }
+        if(result.length == 0){
+            connection.query(insert_query, [nome, email, senha, cnpj, telefone], (err, result) => {
+                if(err){
+                    res.json(err);
+                }
+            })
+            res.status(200).json({message: 'company registered sucessfully', code: 200});
+        }
+        if(result.length > 0){
+            return res.status(409).json({message: 'email already registered', code: 409});
+        }
+
+    });
+})
+
+app.get('/company', (req, res) => {
+    connection.query("SELECT * FROM COMPANY", (err, result) => {
+        if(err){
+            res.write(err);
+        }
+        res.send(JSON.stringify(result))
+    });
+}) 
 
 
 function normalizePort(val) {
